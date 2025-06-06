439ef6baa40e8ee00098397776f7d9195b4a7d87
Robera <roberaasmera@gmail.com>
2025-06-06 19:13:40 +0400

    fix bug

--- backend/firebase/firestore.py
+++ backend/firebase/firestore.py
@@ -1,12 +1,14 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
- 
+from httplib2 import Credentials
 
-credential = credentials.Certificate('./../config/wordmaster-42b21-firebase-adminsdk-fbsvc-9cc735e8a9.json')
+credential: Credentials = credentials.Certificate(
+    "./../config/wordmaster-42b21-firebase-adminsdk-fbsvc-9cc735e8a9.json"
+)
 app = firebase_admin.initialize_app(credential=credential)
 
 db = firestore.client()
 
 
-doc_ref = db.collection('Users').document('rabera')
-doc_ref.set({"first":"RObera", "last":"Abera"}) 
+doc_ref = db.collection("Users").document("rabera")
+doc_ref.set({"first": "RObera", "last": "Abera"})
