9548f8c7409a2329b33aa75d062926cb38b3c121
Robera <roberaasmera@gmail.com>
2025-06-07 21:41:34 +0400

    fix env variables

--- backend/src/firebase/firebase_setup.py
+++ backend/src/firebase/firebase_setup.py
@@ -1,10 +1,24 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
 from httplib2 import Credentials
+import os
+from dotenv import load_dotenv
 
-credential: Credentials = credentials.Certificate(
-    "./../config/wordmaster-42b21-firebase-adminsdk-fbsvc-9cc735e8a9.json"
-)
+load_dotenv()
+
+credential = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
 app = firebase_admin.initialize_app(credential=credential)
 
 db = firestore.client()
